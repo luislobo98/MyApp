98a962b4bac85e8f0b3cdede50f3fac304931d17
luislobo98 <luis.santos629@etec.sp.gov.br>
2023-09-14 21:48:17 -0300

    Imagem Clicável

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -2,13 +2,28 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ImageView imgLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        imgLogin = findViewById(R.id.imgLogin);
+
+        imgLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SplashActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/br/com/etecia/myapp/SplashActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -2,13 +2,33 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    //declarar uma variável global do tipo objeto no layout
+    ImageView imgLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+
+        //Atribuir a variável para o ID
+        imgLogo = findViewById(R.id.imgLogo);
+
+        //Efetivar o botão clicavél na imagem
+        imgLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Indicar onde estou (local.this) e para onde vou (local.class)
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
+        });
+
     }
 }
