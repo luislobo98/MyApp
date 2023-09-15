afbbbbd0bc33db7abe34a7c063a2017fbe4d702a
luislobo98 <luis.santos629@etec.sp.gov.br>
2023-09-14 22:13:25 -0300

    Criando Toast

--- app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
@@ -6,13 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
 
+    MaterialCardView cadAeromodelos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal_layout);
+
+        cadAeromodelos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "Clicou em Aeromodelos", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
