ca355d9af133ad5bad30aee4b8ee4ec1cffed578
luislobo98 <luis.santos629@etec.sp.gov.br>
2023-09-14 21:59:01 -0300

    Imagem Clicável

--- app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
@@ -2,9 +2,13 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
